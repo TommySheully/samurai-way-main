fb29903dc8ecc2a757a98814989b75a4b9949308
Tommy <dfgthii@gmail.com>
2022-09-08 10:40:37 +0300

    first 9 lessons "road the samurai"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import Tehnologies from "./Components/Tehnologies";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <Header/>
+            <Tehnologies/>
+        </div>
+    );
 }
 
 export default App;
