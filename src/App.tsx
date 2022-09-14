b95aa98f6bcae17568226b7b530c422ff204437e
Tommy <dfgthii@gmail.com>
2022-09-14 13:41:59 +0300

    13 lesson for samurai-way

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import Header from "./Components/Header";
+import NavBar from "./Components/NavBar";
+import Profile from "./Components/Profile";
 
 function App() {
     return (
         <div className="App-wrapper">
-            <header className="header">
-                <img src="https://cdn-icons-png.flaticon.com/512/36/36447.png" alt="logo"/>
-            </header>
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className="content">Main content
-                <div>
-                    <img
-                        src="https://sun9-88.userapi.com/impg/Yb3CROCd3_uwduaCMcUTmGdFv58w4HB3VuVnzg/KOylVH72HEo.jpg?size=905x661&quality=96&sign=47ec0b50464108c17a09f3f008bd41a5&type=album"/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My post
-                    <div> New post</div>
-                </div>
-                <div>
-                    post 1
-                </div>
-                <div>
-                    post 2
-                </div>
-            </div>
+            <Header/>
+            <NavBar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://cdn-icons-png.flaticon.com/512/36/36447.png" alt="logo"/>
+        </header>
+    )
+};
+
+export default Header;
